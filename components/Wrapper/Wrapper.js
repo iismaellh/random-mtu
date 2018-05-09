e082d9be860d7acea86b1fb6f829cdd5eeeb4afd
randomtu <ismael13herrera@gmail.com>
2018-05-10 02:44:37 +0800

    adjust blur

--- components/Wrapper/Wrapper.js
+++ components/Wrapper/Wrapper.js
@@ -16,14 +16,20 @@ const wrapper = w({
         opacity: 1,
         transition: `${duration}ms ease-in`,
         filter: `blur(10px)`,
-        transform: `translate(-100vw, 0vh)`
+       // transform: `translate(-100vw, 0vh)`
       }
     },
+    // transitionStyles: {      //transition styles
+    //   exited: { opacity: 1, filter: `blur(5px)`, transform: `translate(0vw, 0vh)` },
+    //   entering: { opacity: 1, filter: `blur(10px)`, transform: `translate(-100vw, 0vh)` },
+    //   entered: { opacity: 1, filter: `blur(0px)`, transform: `translate(0vw, 0vh)` },
+    //   exiting: { opacity: 1, filter: `blue(10px)`, transform: `translate(100vw, 0vh)` }
+    // },
     transitionStyles: {      //transition styles
-      exited: { opacity: 1, filter: `blur(5px)`, transform: `translate(0vw, 0vh)` },
-      entering: { opacity: 1, filter: `blur(10px)`, transform: `translate(-100vw, 0vh)` },
-      entered: { opacity: 1, filter: `blur(0px)`, transform: `translate(0vw, 0vh)` },
-      exiting: { opacity: 1, filter: `blue(10px)`, transform: `translate(100vw, 0vh)` }
+      exited: { opacity: 1, filter: `blur(5px)`, },
+      entering: { opacity: 1, filter: `blur(100px)`, },
+      entered: { opacity: 1, filter: `blur(0px)`, },
+      exiting: { opacity: 1, filter: `blue(10px)`, }
     }
   })
 
